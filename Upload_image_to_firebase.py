5aa2b8eef5faa19dcc5db150a9fbd814c820de52
maitronghuu <maitronghuu@gmail.com>
2023-06-18 03:48:23 +0700

    Update Upload_image_to_firebase.py

--- Upload_image_to_firebase.py
+++ Upload_image_to_firebase.py
@@ -1,10 +1,6 @@
 
 
 #Thư viện hỗ trợ
-import RPi.GPIO as GPIO
-GPIO.setwarnings ( False )
-GPIO.setmode ( GPIO.BOARD )
-GPIO.setup ( 10 , GPIO.IN , pull_up_down=GPIO.PUD_DOWN )
 from datetime import datetime
 from picamera import PiCamera
 from time import sleep
@@ -13,15 +9,14 @@ import pyrebase
 
 #api key firebase
 firebaseConfig = {
-    'apiKey': "AIzaSyA9RPYldW1Bh6FTxKEpvi6ntrHxTEpZeoc" ,
-    'authDomain': "rpi-image-e562d.firebaseapp.com" ,
-    'databaseURL': "https://rpi-image-e562d-default-rtdb.firebaseio.com" ,
-    'projectId': "rpi-image-e562d" ,
-    'storageBucket': "rpi-image-e562d.appspot.com" ,
-    'messagingSenderId': "835517945977" ,
-    'appId': "1:835517945977:web:15e42f3420a70ae9d8dca8" ,
-    'measurementId': "G-HK159X6WJ3"
-
+    'apiKey': "AIzaSyBtbt86NZAGdha9EibXs5N2UXdtRCJ25EA",
+    'authDomain': "tudiendict.firebaseapp.com",
+    'databaseURL': "https://tudiendict-default-rtdb.firebaseio.com",
+    'projectId': "tudiendict",
+    'storageBucket': "tudiendict.appspot.com",
+    'messagingSenderId': "344257519816",
+    'appId': "1:344257519816:web:a60248af82e0b1448585fd",
+    'measurementId': "G-QHEVM1KB3J"
 }
 
 firebase = pyrebase.initialize_app ( firebaseConfig )
@@ -32,8 +27,7 @@ camera = PiCamera ()
 
 while True:
     try:
-        if GPIO.input ( 10 ) == GPIO.HIGH:
-            print ( "pushed" )
+     
             now = datetime.now ()
             dt = now.strftime ( "%d%m%Y%H:%M:%S" )
             name = dt + ".jpg"
@@ -43,7 +37,7 @@ while True:
             print ( "Image sent" )
             os.remove ( name )
             print ( "File Removed" )
-            sleep ( 2 )
+            sleep ( 5 )
 
 
     except:
